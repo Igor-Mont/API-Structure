f74877be2973163bbc1c73381d43d7afffcabe72
Igor Mont <igornathandev@gmail.com>
2022-04-28 14:26:50 -0300

    add jest configs

---
+++ jest-integration-config.js
@@ -0,0 +1,3 @@
+import config from './jest.config.js'
+config.testMatch = ['**/*.test.js']
+export default config
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+export default {
+  roots: ["<rootDir>/src"],
+  // collectCoverageFrom: ['<rootDir>/src/**/*.js'],
+  // coverageDirectory: "coverage",
+  // coverageProvider: "babel",
+  preset: '@shelf/jest-mongodb',
+  testEnvironment: "node",
+};
+
