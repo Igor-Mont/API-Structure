a3fcee67258317c347a42a92d28876a582e2ab4e
Igor Mont <igornathandev@gmail.com>
2022-04-30 00:30:24 -0300

    test userValidation middleware

---
+++ src/middlewares/userValidation.test.js
@@ -0,0 +1,95 @@
+import request from "supertest";
+import { app } from "../app.js";
+
+describe('User Validation Middleware', () => {
+  test('should return a bad request if no data is provided', async () => {
+    const response = await request(app)
+      .post('/users')
+    
+    expect(response.statusCode).toEqual(400) 
+  });
+
+  test('should return a bad request if first_name no is provided or invalid', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        last_name: "any_last_name",
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        age: 18,
+        cpf: 11111111111
+      })
+    
+    expect(response.statusCode).toEqual(400) 
+  });
+
+  test('should return a bad request if last_name no is provided or invalid', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        first_name: "any_first_name",
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        age: 18,
+        cpf: 11111111111
+      })
+    
+    expect(response.statusCode).toEqual(400) 
+  });
+
+  test('should return a bad request if password no is provided or invalid', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        first_name: "any_first_name",
+        last_name: "any_last_name",
+        email: 'any_email@mail.com',
+        age: 18,
+        cpf: 11111111111
+      })
+    
+    expect(response.statusCode).toEqual(400) 
+  });
+
+  test('should return a bad request if email no is provided or invalid', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        first_name: "any_first_name",
+        last_name: "any_last_name",
+        password: 'any_password',
+        age: 18,
+        cpf: 11111111111
+      })
+    
+    expect(response.statusCode).toEqual(400) 
+  });
+
+  test('should return a bad request if age no is provided or invalid', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        first_name: "any_first_name",
+        last_name: "any_last_name",
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        cpf: 11111111111
+      })
+    
+    expect(response.statusCode).toEqual(400) 
+  });
+
+  test('should return a bad request if age no is provided or invalid', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        first_name: "any_first_name",
+        last_name: "any_last_name",
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        age: 18
+      })
+    
+    expect(response.statusCode).toEqual(400) 
+  });
+});
