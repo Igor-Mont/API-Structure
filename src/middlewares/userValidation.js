c6485fc717b2bd6e1446a1e2577885d084de5383
Igor Mont <igornathandev@gmail.com>
2022-05-01 22:42:47 -0300

    refact error message on userValidation

--- src/middlewares/userValidation.js
+++ src/middlewares/userValidation.js
@@ -12,7 +12,7 @@ async function userValidation(req, res, next) {
 
   if (Object.keys(req.body).length === 0) {
     return res.status(400).json({
-      message: "Request request cannot be empty"
+      message: "Request cannot be empty"
     });
   }
 
